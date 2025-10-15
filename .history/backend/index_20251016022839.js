8a229c1ab9b8dd39629454a7051a0cc8969cf68c
Vinit2113 <vinitshah2213@gmail.com>
2025-10-16 02:37:44 +0530

    first commit

---
+++ .history/backend/index_20251016022839.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 4980;
+
+app.get("", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
